d04170cd2f6dd0945bf845bbf19526b0d58c5686
naman-sfs <164490127+naman-sfs@users.noreply.github.com>
2024-08-16 15:53:49 +0530

    first commit

---
+++ Machine Learning/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="project_name",
+    version="0.0.0",
+    author="Naman Rao",
+    author_email="sfs.namanrao24@gmail.com",
+    packages=find_packages()
+)
